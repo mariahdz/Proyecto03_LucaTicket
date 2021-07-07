74619b649b87c2a0e2aa08dd2bf221fc2b6c0500
mariahdz <mariahdeztejero@gmail.com>
2021-07-07 10:02:44 +0200

    Controller addEvento

--- Proyecto03_LucaTicket_GestionEventos/src/main/java/com/proyectos/grupo01/controller/EventoController.java
+++ Proyecto03_LucaTicket_GestionEventos/src/main/java/com/proyectos/grupo01/controller/EventoController.java
@@ -1,5 +1,41 @@
 package com.proyectos.grupo01.controller;
 
-public class EventoController {
+import org.jboss.logging.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
+import com.proyectos.grupo01.model.Evento;
+import com.proyectos.grupo01.repository.EventoRepository;
+
+
+/**
+ * 
+ * @author María Hernández
+ * 
+ *
+ */
+
+@RestController
+public class EventoController {
+	
+	private static final Logger log = Logger.getLogger("EventoRepositoryImpl.class");
+
+	
+	@Autowired
+	EventoRepository eventoRepository;
+	
+	
+	@PostMapping("/save")
+	public ResponseEntity <Evento> addEvento (@RequestBody Evento eventoRequest) {
+		
+		log.infof("Request: ",eventoRequest);
+		
+		Evento evento = (Evento) eventoRepository.save(eventoRequest);
+		return new ResponseEntity <> (evento, HttpStatus.CREATED);
+		
+	}
 }
--- Proyecto03_LucaTicket_GestionEventos/src/main/java/com/proyectos/grupo01/repository/EventoRepository.java
+++ Proyecto03_LucaTicket_GestionEventos/src/main/java/com/proyectos/grupo01/repository/EventoRepository.java
@@ -1,9 +1,11 @@
 package com.proyectos.grupo01.repository;
 
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import com.proyectos.grupo01.model.Evento;
 
 public interface EventoRepository extends MongoRepository<Evento,String>{
+
 
 }
