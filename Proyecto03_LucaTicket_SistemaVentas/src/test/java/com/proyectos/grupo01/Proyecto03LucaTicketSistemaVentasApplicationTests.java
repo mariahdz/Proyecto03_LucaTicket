5b2a1d30f7bdedae6faa5169485f6b7950666047
unknown <desrodgar@gmail.com>
2021-07-13 15:39:11 +0200

    cambios

--- Proyecto03_LucaTicket_SistemaVentas/src/test/java/com/proyectos/grupo01/Proyecto03LucaTicketSistemaVentasApplicationTests.java
+++ Proyecto03_LucaTicket_SistemaVentas/src/test/java/com/proyectos/grupo01/Proyecto03LucaTicketSistemaVentasApplicationTests.java
@@ -1,13 +1,23 @@
 package com.proyectos.grupo01;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.proyectos.grupo01.controller.VentaController;
 
 @SpringBootTest
 class Proyecto03LucaTicketSistemaVentasApplicationTests {
 
+	@Autowired
+	VentaController controller;
+	
 	@Test
-	void contextLoads() {
+	void testController() {
+		
+		assertThat(controller).isNotNull();
+		
 	}
-
 }
---
+++ Proyecto03_LucaTicket_SistemaVentas/src/test/java/com/proyectos/grupo01/VentaTest.java
@@ -0,0 +1,25 @@
+package com.proyectos.grupo01;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.proyectos.grupo01.controller.VentaController;
+import com.proyectos.grupo01.model.Evento;
+import com.proyectos.grupo01.model.Usuario;
+
+class VentaTest {
+	
+//	@Autowired
+//	VentaController control;
+//
+//	@Test
+//	void test() {
+//		Evento evento = new Evento();
+//		Usuario usuario = new Usuario();
+//		
+//		when(control.comprobarVenta("urlAntonio", evento.getId(), usuario.getNombre()));
+//	}
+
+}
